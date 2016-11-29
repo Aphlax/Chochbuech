97fd5707ab7afb083c4dd5713526b23aac1e67fb
Fabian Meier <meiefabi@student.ethz.ch>
2016-11-29 23:10:20 +0000

    added camera test, debugging

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,29 +11,46 @@
     ];
 
     angular.module('Chochbuech', REQ)
-        .directive('main-site', function() {
-            return {restrict: 'E', replace: true, templateUrl: 'templates/main-site.html'}
+        .directive('photoSite', function() {
+            return { restrict: 'E', replace: true, templateUrl: 'templates/photo-site.html' }
         })
-        .controller('main', ['$scope', '$http', function($scope, $http) {
+        .controller('photo', ['$scope', '$http', '$element', function($scope, $http, $element) {
+
+            let video = $element.find('video')[0];
+            let canvas = $element.find('canvas')[0];
+            let image = $element.find('img')[0];
 
             $scope.images = [];
+            $scope.mode = 0;
 
-            $http.get('/image-list', function(res, data) {
-                $scope.images = data;
-            });
-
+            refresh();
 
 
+            $scope.take = function () {
+                let context = canvas.getContext('2d');
+                context.drawImage(video, 0, 0, 200, 200);
+                let data = canvas.toDataURL('image/png');
+                image.setAttribute('src', data);
+
+                try {
+                    $http.post('/addImage', data);
+                } catch (e) {$scope.error = e.message;}
+            };
+
+            function refresh() {
+                $http.get('/image-list').then(function (res) {
+                    $scope.images = res.data;
+                });
+            }
 
 
+            let options = { audio: false, video: { facingMode: { exact: "environment" } } };
 
-
-
-
-            navigator.mediaDevices.getUserMedia({ audio: false, video: { facingMode: "environment" } })
+            navigator.mediaDevices.getUserMedia(options)
                 .then(function(stream) {
-
-                }).catch(e => console.log(e));
+                    video.src = window.URL.createObjectURL(stream);
+                    video.play();
+                }).catch(e => $scope.error = e.message);
         }])
 })();
 
--- server.js
+++ server.js
@@ -13,14 +13,57 @@ if (!Array.prototype.includes)
 
 let express = require('express');
 let app = express();
-let http = require('http').Server(app);
+let http = require('http');
+let https = require('https');
 let mongo = require('mongodb').MongoClient;
+let fs = require('fs');
+
+let sec = { key: fs.readFileSync('key.pem'), cert: fs.readFileSync('cert.pem') };
 
 app.use(express.static('public'));
 app.get('/', function(req, res) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     res.sendFile(__dirname + '/public/index.html');
 });
 
-http.listen(3001, "0.0.0.0", function() {
-    console.log('listening on 3001');
+let db = null;
+mongo.connect('mongodb://localhost:27017/ChochbuechDB', function(err, database) {
+    if(err) console.error(err);
+    db = database;
 });
+
+
+
+app.get('/image-list', function(req, res) {
+    db.collection('images').find({}, { _id: 1 }).toArray(function(e, data) {
+        res.json(data.map(img => { return { id: img._id }; })).end();
+    });
+});
+app.get('/images/:id', function(req, res) {
+    db.collection('images').find({ _id: req.params.id }).toArray(function(e, data) {
+        res.json(data[0].data).end();
+    });
+});
+app.post('/addImage', function(req, res) {
+
+    db.collection('images').insertMany([{ data: 9 }])
+        .then(() => res.status(200).end());
+});
+
+
+let httpServer = http.createServer(app);
+let httpsServer = https.createServer(sec, app);
+httpServer.listen(3001);
+httpsServer.listen(3002);
+
+
+
+
+
+
+
+
+
+
+
+
