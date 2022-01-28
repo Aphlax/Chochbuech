3b32863bcaebfe89d65d6f744e02d72aab322fa8
Fabian Meier <fabian.91m@gmail.com>
2022-01-28 18:41:22 +0100

    Added support for links in recipe notes.

--- public/scripts/values.js
+++ public/scripts/values.js
@@ -63,23 +63,43 @@
 
             return new RecipeService();
         }])
-        .value('recipeDisplay', function(recipe) {
-            const ingredientGroups = recipe.ingredients.split('-- ').map((g, i) => g ? ({
-                name: i ? g.substring(0, g.indexOf('\n')) : '',
-                items: g.split('\n').filter((item, j) => item && (!i || j)),
-            }) : undefined).filter(g => g);
+        .factory('recipeDisplay', ['parseConnectedString', function(parseConnectedString) {
+            return function (recipe) {
+                const ingredientGroups = recipe.ingredients.split('-- ').map((g, i) => g ? ({
+                    name: i ? g.substring(0, g.indexOf('\n')) : '',
+                    items: g.split('\n').filter((item, j) => item && (!i || j)),
+                }) : undefined).filter(g => g);
 
-            const prepText = 'Vorbereitung: ';
-            const prepIndex = recipe.steps.startsWith(prepText) ? recipe.steps.indexOf('\n') : 0;
-            let noteIndex = recipe.steps.indexOf('\n\n');
-            if (noteIndex == -1) noteIndex = recipe.steps.length;
-            return {
-                ingredientGroups,
-                preparation: prepIndex ? recipe.steps.substring(prepText.length, prepIndex) : '',
-                steps: recipe.steps.substring(prepIndex, noteIndex).split('\n').filter(i => i),
-                notes: recipe.steps.substring(noteIndex + 2),
+                const prepText = 'Vorbereitung: ';
+                const prepIndex = recipe.steps.startsWith(prepText) ? recipe.steps.indexOf('\n') : 0;
+                let noteIndex = recipe.steps.indexOf('\n\n');
+                if (noteIndex == -1) noteIndex = recipe.steps.length;
+                return {
+                    ingredientGroups,
+                    preparation: prepIndex ? recipe.steps.substring(prepText.length, prepIndex) : '',
+                    steps: recipe.steps.substring(prepIndex, noteIndex).split('\n').filter(i => i),
+                    notes: parseConnectedString(recipe.steps.substring(noteIndex + 2)),
+                }
+            };
+        }])
+        .value('parseConnectedString', function(value) {
+            let regex = /([^[]+)(?:\[([^\]]+)]\(([^)]*)\))?/g, match, result = [];
+            while (match = regex.exec(value)) {
+                result.push({type: 0, value: match[1]});
+                if (match[2]) result.push({type: 1, value: match[2], href: match[3]});
             }
+            return result;
         })
+        .directive('connectedString', [function() {
+            return {
+                restrict: 'E',
+                replace: false,
+                scope: { value: '=' },
+                template:
+                    '<span ng-repeat-start="it in value" ng-if="it.type == 0">{{it.value}}</span>' +
+                    '<a ng-repeat-end ng-if="it.type == 1" href="{{it.href}}">{{it.value}}</a>',
+            };
+        }])
         .factory('properties', ['$http', function($http) {
             const properties = { canEdit: false };
             $http.get('/properties').then(({status, data}) => {
